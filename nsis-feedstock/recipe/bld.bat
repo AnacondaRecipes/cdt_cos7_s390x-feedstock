cfed6cd6c5c584d4cad37e6f947d142129b6389d
Kai Tietz <ktietz@anaconda.com>
2021-01-16 12:09:35 +0100

    Update sysroot for s390x (2 of 2)

--- nsis-feedstock/recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-robocopy nsis-zip "%PREFIX%"\NSIS /S
-robocopy "%RECIPE_DIR%"\NSIS "%PREFIX%"\NSIS /S
-:: Checking the ErrorLevel from robocopy is pointless.
-exit /b 0
--- serf-feedstock/recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sed -i.bak 's/c89/c99/g' SConstruct
-
-scons APR="$PREFIX" APU="$PREFIX" OPENSSL="$PREFIX" ZLIB="$PREFIX" \
-    PREFIX="$PREFIX" CC="$CC" CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
-
-scons install
